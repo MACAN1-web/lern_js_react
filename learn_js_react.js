33e85fe7bf66c4814a4a781be30bf52422fff13b
Vlad Lutskin <lutskinvlad2008@gmail.com>
2026-02-28 20:58:21 +0300

    add new dz

--- learn_js_react.js
+++ learn_js_react.js
@@ -11,7 +11,38 @@
 /*
     Работа с цыклом
 */
-let num = 3;
-for(let i = 1; i <= 10; i++){
-    console.log(`${num} * ${i} = ${num * i}`)
-}
+// let num = 3;
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${num} * ${i} = ${num * i}`)
+// }
+
+/*
+Создаем генератор случаного пароля  
+*/
+// function fn (p){
+//     const pass =
+//     "qazxswcedvrHBUVYKMOLT648329#%*(*!@#())"
+//     let password ='';
+
+//     for (let i = 0; i<p ;i++){
+//         const paas = Math.floor(Math.random() * pass.length);
+//         password += pass[paas];
+//     }
+//     return password;
+// }
+// const dwe = fn(90);
+// console.log(dwe)
+//
+/*
+ Стрелочная функция 
+*/
+// function isEven (number){
+//     return number % 2 === 0 ? 'Четное':'нечетное';
+// }
+// let bro = isEven(7)
+// console.log(bro)
+
+// function calculateArea (a1, a2){
+//     return a1 * a2
+// }
+// console.log(calculateArea(12, 56))
