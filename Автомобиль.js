521548f21462140a3fb43ff448c9931ba4c31bc1
Vlad Lutskin <lutskinvlad2008@gmail.com>
2026-03-04 11:56:02 +0300

    new dz

---
+++ Автомобиль.js
@@ -0,0 +1,21 @@
+const car = {
+  brand: "",
+  model: "",
+  year: 0,
+  displayInfo() {
+    console.log(`Марка: ${this.brand}, Модель: ${this.model}, Год: ${this.year}`);
+  },
+};
+
+const Camry = Object.create(car);
+Camry.brand = "Toyota";
+Camry.model = "Camry";
+Camry.year = 2020;
+
+const Mustang = Object.create(car);
+Mustang.brand = "Ford";
+Mustang.model = "Mustang";
+Mustang.year = 2018;
+
+Camry.displayInfo();
+Mustang.displayInfo();
