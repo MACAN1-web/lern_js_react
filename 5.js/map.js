a3965aaab6d149ba4de0f2a6502d594079e6eff0
Vlad Lutskin <lutskinvlad2008@gmail.com>
2026-03-06 09:17:13 +0300

    add new dz

---
+++ 5.js/map.js
@@ -0,0 +1,14 @@
+// const numbers = [1, 2, 3, 4, 5];
+// const b = [];
+// numbers.forEach((num) => {
+//   return b.push(num * num);
+// });
+// console.log(b);
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map((sq) => sq * sq)
+
+console.log(squares)
+
---
+++ 5.js/Квадрат_числа.js
@@ -0,0 +1,5 @@
+const arr = [2, 3, 4, 5];
+
+arr.forEach((num) => {
+  console.log(`Квадрат числа ${num}: ${num * num}`);
+});
